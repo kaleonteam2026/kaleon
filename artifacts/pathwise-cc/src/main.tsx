ffa57ce02b4187f74eb4e73b970f84d430833af0
koms24 <omaribrahim@csus.edu>
2026-05-27 22:37:40 -0700

    error fix

--- artifacts/pathwise-cc/src/main.tsx
+++ artifacts/pathwise-cc/src/main.tsx
@@ -2,8 +2,8 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import "./i18n/config";
-import { installLocaleFetch } from "./i18n/fetch-locale";
 
-installLocaleFetch();
+
+
 
 createRoot(document.getElementById("root")!).render(<App />);
