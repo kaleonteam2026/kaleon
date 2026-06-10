bee277da223aa0c3f185817701f652f40d69bacc
koms24 <omaribrahim@csus.edu>
2026-06-10 15:47:58 -0700

    mobile indent fix

--- src/pages/courses.tsx
+++ src/pages/courses.tsx
@@ -380,9 +380,7 @@ export default function Courses() {
               <Plus className="h-4 w-4 mr-2" /> Manual
             </Button>
           )}
-          <Button onClick={openCatalog} className="bg-slate-900 hover:bg-slate-700 text-white border-2 border-slate-900 rounded-none flex-shrink-0">
-            <Plus className="h-4 w-4 mr-2" /> Catalog
-          </Button>
+
           <Button
             onClick={() => setShowReuploadConfirm(true)}
             variant="outline"
